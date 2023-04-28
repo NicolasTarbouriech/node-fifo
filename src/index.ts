1daf486715cdefb46eeafa7b7645457128417f86
Nicolas Tarbouriech <tarboun@hotmail.fr>
2023-04-28 18:11:23 +0200

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import express, { Request, Response } from 'express';
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.get("/api", (req: Request, res: Response) => {
+  let json = {
+    message: 'sq'
+  }
+  res.json(json);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
